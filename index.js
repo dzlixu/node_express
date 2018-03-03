06af61035c3a0a0697bb119d8ceb9a1c9bd89830
dzlixu <dzlixu@126.com>
2018-03-03 12:32:47 +0800

    chap5

--- index.js
+++ index.js
@@ -8,24 +8,42 @@ app.set('view engine', 'handlebars');
 
 
 // 设置端口
-app.set('port',process.env.PORT || 3300);
+app.set('port', process.env.PORT || 3300);
 
 // 设置静态目录
-app.use(express.static( __dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 
+
+
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
 
 app.get('/', function (req, res) {
     res.render('home');
 
 });
 app.get('/about', function (req, res) {
-    res.render('about',{fortune:fortune.getFortune()});
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    });
 });
+
+app.get('/tours/hood-river', function (req, res) {
+    // res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function (req, res) {
+    res.render('/tours/request-group-rate');
+});
+
 app.get('/headers', function (req, res) {
     res.type('html');
-    var s ='';
-    for(var name in req.headers){
-        s+='<div>'+name+':'+req.headers[name]+'\n'+'</div>';
+    var s = '';
+    for (var name in req.headers) {
+        s += '<div>' + name + ':' + req.headers[name] + '\n' + '</div>';
     }
     res.send(s);
 });
@@ -35,7 +53,7 @@ app.use(function (req, res) {
     res.render('404');
 });
 
-app.use(function (err,req, res) {
+app.use(function (err, req, res) {
     console.log(err.stack);
     res.status(500);
     res.render('500');
